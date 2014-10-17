5204de4ab100c3d155c9c388d07ec198d3fd2500
pethO <petho@temnota.sk>
2014-10-17 19:19:27 +0200

    ability to configure hostname of AD

--- FootballLeague/Services/UsersADSearcher.cs
+++ FootballLeague/Services/UsersADSearcher.cs
@@ -2,6 +2,8 @@
 using System.Web;
 using FootballLeague.Models;
 using System.DirectoryServices;
+using System;
+using System.Configuration;
 
 namespace FootballLeague.Services
 {
@@ -9,23 +11,30 @@ namespace FootballLeague.Services
     {
         public User LoadUserDetails(string userName)
         {
-            var entry = new DirectoryEntry();
-            var searcher = new DirectorySearcher(entry);
+			var hostName = ConfigurationManager.AppSettings.Get ("adHostName");
+			var adPath = "";
+			if (!string.IsNullOrEmpty (hostName)) 
+			{
+				adPath = string.Format ("Ldap://{0}:389", hostName);
+			}
+			var entry = new DirectoryEntry(adPath);
+			var searcher = new DirectorySearcher(entry);
             searcher.Filter = "(&(objectClass=user)(sAMAccountName=" + userName + "))";
             searcher.PropertiesToLoad.Add("givenName");
             searcher.PropertiesToLoad.Add("sn");
             searcher.PropertiesToLoad.Add("mail");
             try
             {
-                var userProps = searcher.FindOne().Properties;
+				var userProps = searcher.FindOne().Properties;
                 var mail = userProps["mail"][0].ToString();
                 var first = userProps["givenName"][0].ToString();
                 var last = userProps["sn"][0].ToString();
 
                 return new User { Name = userName, Mail = mail, FirstName = first, LastName = last };
             }
-            catch
+			catch(Exception e)
             {
+				Console.WriteLine (e.Message + Environment.NewLine + e.StackTrace);
 				return new User { Name = userName, Mail = "local@user.sk", FirstName = "Local", LastName = "User" };
             }
         }
